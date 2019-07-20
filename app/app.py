78854468865b843703f730ce42c0c82c493d4938
leehanchung <kein520@yahoo.com>
2019-07-20 01:36:20 -0700

    init

---
+++ app/app.py
@@ -0,0 +1,10 @@
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+import plotly.figure_factory as ff
+import plotly.graph_objs as go
+import pandas as pd
+import numpy as np
+import colorlover
+
