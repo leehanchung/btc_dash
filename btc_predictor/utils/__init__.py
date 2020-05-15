552dde2a343b13f28bcbf0afe50ff3dd5fe100a9
leehanchung <kein520@yahoo.com>
2020-05-14 22:48:55 -0700

    feat: add walkforward charts refactor: move common functions to utils refactor: clean up notebooks

--- btc_predictor/utils/__init__.py
+++ btc_predictor/utils/__init__.py
@@ -2,4 +2,6 @@
 from .utils import preproc
 from .utils import cv_score
 from .utils import get_current_time
+from .utils import print_metrics
+from .utils import show_plot
 from .PurgedKFold import PurgedKFold
