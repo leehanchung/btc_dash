21824e793022f9b294e777a6315e19e7b29877da
leehanchung <kein520@yahoo.com>
2020-12-09 23:42:10 -0800

    docker-compose backend working

--- backend/app/routes/api.py
+++ backend/app/routes/api.py
@@ -1,20 +1,20 @@
-# from flask import Blueprint, render_template, abort
-# from flask_restx import Api, Namespace, Resource, fields
+from flask import Blueprint
+from flask_restx import Api, Namespace, Resource
 
 
-# blueprint = Blueprint('btcdash',
-#                       __name__,
-#                       template_folder='templates')
-# api = Api(blueprint,
-#           version='0.1',
-#           title='BTC_USD API',
-#           description="BTC_USD API for BTC_Dash")
+blueprint = Blueprint('btcdash',
+                      __name__,
+                      template_folder='templates')
+api = Api(blueprint,
+          version='0.1',
+          title='BTC_USD API',
+          description="BTC_USD API for BTC_Dash")
 
-# namespace = api.namespace('api',
-#                           description="OHLCV + Predict")
+namespace = api.namespace('api',
+                          description="OHLCV + Predict")
 
-# @namespace.route('/')
-# class Health(Resource):
-#     # @api.doc(description="health checkup")
-#     def get(self):
-#         return {'status': 'ok', 'version': api.version}
+@namespace.route('/')
+class Health(Resource):
+    # @api.doc(description="health checkup")
+    def get(self):
+        return {'status': 'ok', 'version': api.version}
