30c3a694d3d2a4f244361a5e9005b04481322115
leehanchung <lee.hanchung@gmail.com>
2021-06-14 23:53:29 -0700

    added dockerfile and config

---
+++ btc_predictor/btc_predictor/config/config.py
@@ -0,0 +1,29 @@
+import pathlib
+import random
+
+import numpy as np
+import pandas as pd
+import btc_predictor
+
+
+random.seed(78)
+np.random.seed(78)
+pd.options.display.max_rows = None
+pd.options.display.max_columns = 500
+
+
+class Config:
+    PACKAGE_ROOT = (
+        pathlib.Path(approval_odds_predictor.__file__).resolve().parent
+    )
+    DATASET_DIR = PACKAGE_ROOT / "datasets"
+
+    AWS_ACCESS_KEY_ID = ""
+    AWS_SECRET_ACCESS_KEY = ""
+    BUCKET = ""
+
+    RANDOM_STATE = 78
+    TRACKING_URI = ""
+
+
+config = Config()
