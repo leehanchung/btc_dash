962570fd4863d769da00789b73a5f6d9acb28bf0
leehanchung <lee.hanchung@gmail.com>
2021-07-10 23:22:21 -0700

    :lipstick: isort

--- btc_predictor/btc_predictor/utils/__init__.py
+++ btc_predictor/btc_predictor/utils/__init__.py
@@ -1,4 +1,11 @@
 # flake8: noqa
 from .PurgedKFold import PurgedKFold
-from .utils import (calculate_metrics, cv_score, get_current_time,
-                    plot_train_history, preproc, print_metrics, show_plot)
+from .utils import (
+    calculate_metrics,
+    cv_score,
+    get_current_time,
+    plot_train_history,
+    preproc,
+    print_metrics,
+    show_plot,
+)
